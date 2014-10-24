b98ff519bf979c911048085e31cc3e0fe6a6776d
Andrew Larson <andrew.p.larson@gmail.com>
2014-10-23 18:06:42 -0700

    Make horrible single query sibling responses

--- app/models/epic_query.sql
+++ app/models/epic_query.sql
@@ -34,5 +34,26 @@ FROM (
 )
 
 SELECT
-
+ *
 FROM
+responses AS a_responses
+JOIN
+answer_choices AS a_choices
+ON
+a_responses.answer_choice_id = a_choices.id
+JOIN
+questions 
+ON 
+a_choices.question_id = questions.id
+JOIN
+answer_choices AS b_choices
+ON
+questions.id = b_choices.question_id
+JOIN
+responses AS b_responses
+ON
+b_choices.id = b_responses.answer_choice_id
+WHERE
+a_responses.id != 12
+AND
+b_responses.id = 12;
--- app/models/response.rb
+++ app/models/response.rb
@@ -31,11 +31,35 @@ class Response < ActiveRecord::Base
 	through: :answer_choice,
 	source: :question
 	)
-	
-	private
+
 	
 	def sibling_responses
 		question.responses.where('? IS NULL OR responses.id != ?', self.id, self.id)
+	end
+	
+	def sibling_responses2
+		Response.select('a_responses.* FROM
+										responses AS a_responses
+										JOIN
+										answer_choices AS a_choices
+										ON
+										a_responses.answer_choice_id = a_choices.id
+										JOIN
+										questions 
+										ON 
+										a_choices.question_id = questions.id
+										JOIN
+										answer_choices AS b_choices
+										ON
+										questions.id = b_choices.question_id
+										JOIN
+										responses AS b_responses
+										ON
+										b_choices.id = b_responses.answer_choice_id
+										WHERE
+										a_responses.id != 12
+										AND
+										b_responses.id = 12;')
 	end
 	
 	def respondent_has_not_already_answered_question
